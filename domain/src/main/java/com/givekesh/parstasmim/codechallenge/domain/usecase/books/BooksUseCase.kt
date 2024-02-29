598a4bebfb8d010e3cb2647173d01f787cf9e1c2
ahmadgivekesh <ahmadgivekesh@gmail.com>
2024-03-01 01:09:10 +0330

    implement BooksUseCase

---
+++ domain/src/main/java/com/givekesh/parstasmim/codechallenge/domain/usecase/books/BooksUseCase.kt
@@ -0,0 +1,9 @@
+package com.givekesh.parstasmim.codechallenge.domain.usecase.books
+
+import com.givekesh.parstasmim.codechallenge.domain.model.book.response.Book
+import com.givekesh.parstasmim.codechallenge.domain.util.DataState
+import kotlinx.coroutines.flow.Flow
+
+interface BooksUseCase {
+    fun getBooks(): Flow<DataState<List<Book>>>
+}
