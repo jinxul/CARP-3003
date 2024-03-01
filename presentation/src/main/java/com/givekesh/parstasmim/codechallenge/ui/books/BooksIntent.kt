933d69b206e57665702d0a755238a27b93f18ec1
ahmadgivekesh <ahmadgivekesh@gmail.com>
2024-03-02 00:14:56 +0330

    implement add book in BooksFragment

--- presentation/src/main/java/com/givekesh/parstasmim/codechallenge/ui/books/BooksIntent.kt
+++ presentation/src/main/java/com/givekesh/parstasmim/codechallenge/ui/books/BooksIntent.kt
@@ -1,7 +1,10 @@
 package com.givekesh.parstasmim.codechallenge.ui.books
 
+import com.givekesh.parstasmim.codechallenge.domain.model.book.request.BookRequest
+
 sealed class BooksIntent {
     data object GetBooks : BooksIntent()
     class SearchBook(val searchQuery: String) : BooksIntent()
     class DeleteBook(val bookId: String) : BooksIntent()
+    class AddBook(val request: BookRequest) : BooksIntent()
 }
