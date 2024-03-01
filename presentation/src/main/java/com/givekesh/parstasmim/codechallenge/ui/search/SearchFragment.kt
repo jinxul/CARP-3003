a4297bc23f9718a2c4b25a3ea6c5fe4c39d9c8da
ahmadgivekesh <ahmadgivekesh@gmail.com>
2024-03-01 20:15:14 +0330

    change settings menu to search (for navigating to search fragment)

--- presentation/src/main/java/com/givekesh/parstasmim/codechallenge/ui/search/SearchFragment.kt
+++ presentation/src/main/java/com/givekesh/parstasmim/codechallenge/ui/search/SearchFragment.kt
@@ -2,11 +2,16 @@ package com.givekesh.parstasmim.codechallenge.ui.search
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.material3.MaterialTheme
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.givekesh.parstasmim.codechallenge.R
 import com.givekesh.parstasmim.codechallenge.databinding.FragmentSearchBinding
 import com.givekesh.parstasmim.codechallenge.ui.books.BooksViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -38,8 +43,30 @@ class SearchFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        requireActivity().addMenuProvider(menuProvider, viewLifecycleOwner)
+    }
+
+    private val menuProvider = object : MenuProvider {
+        override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        }
+
+        override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+            return false
+        }
+
+        override fun onPrepareMenu(menu: Menu) {
+            val menuItem = menu.findItem(R.id.action_search)
+            if (menuItem != null)
+                menuItem.isVisible = false
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
+        requireActivity().removeMenuProvider(menuProvider)
         _binding = null
     }
 }
