f62571178eadcce91e1fea882efaa4ab48bb2a4d
ahmadgivekesh <ahmadgivekesh@gmail.com>
2024-03-01 20:56:04 +0330

    add deleteBook api

--- data/src/main/java/com/givekesh/parstasmim/codechallenge/data/source/remote/repository/books/BooksRepository.kt
+++ data/src/main/java/com/givekesh/parstasmim/codechallenge/data/source/remote/repository/books/BooksRepository.kt
@@ -1,7 +1,10 @@
 package com.givekesh.parstasmim.codechallenge.data.source.remote.repository.books
 
 import com.givekesh.parstasmim.codechallenge.data.entity.book.response.BookResponse
+import com.givekesh.parstasmim.codechallenge.data.entity.book.response.ResultMessageResponse
 
 interface BooksRepository {
     suspend fun getBooks(): List<BookResponse>
+    suspend fun deleteBook(id: String): ResultMessageResponse
+
 }
--- data/src/main/java/com/givekesh/parstasmim/codechallenge/data/source/remote/repository/books/BooksRepositoryImpl.kt
+++ data/src/main/java/com/givekesh/parstasmim/codechallenge/data/source/remote/repository/books/BooksRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.givekesh.parstasmim.codechallenge.data.source.remote.repository.books
 
 import com.givekesh.parstasmim.codechallenge.data.entity.book.response.BookResponse
+import com.givekesh.parstasmim.codechallenge.data.entity.book.response.ResultMessageResponse
 import com.givekesh.parstasmim.codechallenge.data.source.remote.api.BooksApi
 import retrofit2.HttpException
 import javax.inject.Inject
@@ -10,6 +11,15 @@ internal class BooksRepositoryImpl @Inject constructor(
 ) : BooksRepository {
     override suspend fun getBooks(): List<BookResponse> {
         val response = booksApi.getBooks()
+        val body = response.body()
+        return when {
+            response.isSuccessful && body != null -> body
+            else -> throw HttpException(response)
+        }
+    }
+
+    override suspend fun deleteBook(id: String): ResultMessageResponse {
+        val response = booksApi.deleteBook(id)
         val body = response.body()
         return when {
             response.isSuccessful && body != null -> body
